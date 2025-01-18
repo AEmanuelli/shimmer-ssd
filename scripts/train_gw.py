7a8494eef3be2f3a37cf560043fe6068c81b09db
Alexis Emanuelli <accent.cartilage0r@icloud.com>
2025-01-18 15:18:10 +0100

    Update train_gw.py

--- scripts/train_gw.py
+++ scripts/train_gw.py
@@ -2,4 +2,4 @@ from shimmer_ssd import PROJECT_DIR
 from shimmer_ssd.cli.train_gw import train_gw
 
 if __name__ == "__main__":
-    train_gw(PROJECT_DIR / "config")
+    train_gw(PROJECT_DIR / "config_template")
